8c53c49a8c2fe3750218f7ba94ffe51f7e266829
Sonali Jindal <130656646+j-sonali2003@users.noreply.github.com>
2024-05-08 12:26:22 +0530

    Update setup.py

--- Topsis Assignment/setup.py
+++ Topsis Assignment/setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setup(name="Topsis_Sonali_Jindal_102116117",version="0.19",
-description="This is a topsis package of Deepankar Varma version 0.19",
+description="This is a topsis package of Sonali Jindal version 0.19",
 long_description=long_description,
     long_description_content_type="text/markdown",
 author="Sonali Jindal",
-author_email="satiwkdprhrit@gmail.com",
+author_email="sonalijindal4141@gmail.com",
 packages=['Topsis_Sonali_Jindal_102116117'],
 install_requires=['pandas'],
 include_package_data=True,
